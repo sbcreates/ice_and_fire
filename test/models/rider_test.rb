af52579591d0f780d62717e86ba26c6142c9a530
sbcreates <drawsarahb@gmail.com>
2017-09-19 13:34:10 -0400

    Fixed rider test so that it passes

--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,11 +5,11 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
-    aegon = nil
-    daenerys = nil
+    aegon = Rider.create(name: "Aegon", sword: "Blackfyre")
+    daenerys = Rider.create(name: "Daenerys", sword: "Lightbringer")
 
     # can't change the assert_ statements!
-    skip unless aegon && daenerys
+    # skip unless aegon && daenerys
     assert_equal('Daenerys brings Lightbringer crashing down!', daenerys.swing)
     assert_equal('Aegon brings Blackfyre crashing down!', aegon.swing)
   end
